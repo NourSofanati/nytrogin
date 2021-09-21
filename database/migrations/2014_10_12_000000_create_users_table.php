58bc5fba500bf20794496bad5a53b6f61ee636e9
Nour Sofanati <ultradraftns@gmail.com>
2021-09-21 10:14:33 +0300

    Updated Users phone number to be not unique and fixed a typo in DatabaseSeeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('phone_number')->unique()->nullable();
+            $table->string('phone_number')->nullable();
             $table->string('otp')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
